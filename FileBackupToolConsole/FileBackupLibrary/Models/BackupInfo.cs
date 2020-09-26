0c275a1ec5b0b8a96a9c6cc955b8620e416c083b
iAliJ <47180374+iAliJ@users.noreply.github.com>
2020-09-26 17:36:34 +0300

    Add methods to BackupServices

--- FileBackupToolConsole/FileBackupLibrary/Models/BackupInfo.cs
+++ FileBackupToolConsole/FileBackupLibrary/Models/BackupInfo.cs
@@ -11,5 +11,21 @@ namespace FileBackupLibrary.Models
         public int MaxBackup { get; set; }  // Number of backups should not exceed this number
         public int BackupInterval { get; set; } // Backup interval in minutes
         public DateTime BackUpTimer { get; set; } // Time until the next backup
+        public bool CanBackup
+        {
+            get
+            {
+                return NumberOfBackups <= MaxBackup;
+            }
+        }
+
+        public BackupInfo()
+        {
+            this.BackupLocation = @"C:\Backup";
+            this.NumberOfBackups = 0;
+            this.MaxBackup = 5;
+            this.BackupInterval = 15;
+            this.BackUpTimer = new DateTime(0, 0, 0, 0, this.BackupInterval, 0);
+        }
     }
 }
--- FileBackupToolConsole/FileBackupLibrary/Services/BackupServices.cs
+++ FileBackupToolConsole/FileBackupLibrary/Services/BackupServices.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using DirectoryHelperCL;
+using FileBackupLibrary.Models;
 
 namespace FileBackupLibrary.Services
 {
     public class BackupServices
     {
+        private void BackupFile(Models.FileInfo fileInfo)
+        {
+            // Basically copy a file from one location to another
+            File.Copy(fileInfo.FilePath, fileInfo.FileBackupInfo.BackupLocation);
+        }
+
+        public void DeleteBackupFile(Models.FileInfo fileInfo)
+        {
+            File.Delete(fileInfo.FilePath);
+        }   
+        
+        public void PerformBackup(Models.FileInfo fileInfo)
+        {
+            if(fileInfo.FileBackupInfo.CanBackup)
+            {
+                BackupFile(fileInfo);
+                fileInfo.FileBackupInfo.NumberOfBackups++;
+            }
+        }
     }
 }
